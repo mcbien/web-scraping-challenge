fa9c40ea1056b2b10690da1dfc795380cd7864fe
mcbien <mcbien1122@gmail.com>
2020-10-28 19:52:17 -0500

    Saving homework progress

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -35,7 +35,9 @@ def scrape_news():
 def scrape():
     news_title, news_p = scrape_news()
     large_image_link = scrape_image_otd()
-    return {"news_title": news_title, "news_text": news_p, "large_img_link": large_image_link}
+    hemisphere_image_urls = scrape_hemi_urls()
+    return {"news_title": news_title, "news_text": news_p, "large_img_link": large_image_link, "hemi_urls": hemisphere_image_urls}
+###############################
 
 def scrape_image_otd():
     # Define url to scrape
@@ -69,18 +71,72 @@ def scrape_image_otd():
 
     return large_image_link
 
-    def scrape_facts():
-        # Mars Facts with pandas scraping
-        # Set url
-        url = "https://space-facts.com/mars/"
-        factspage = pd.read_html(url)
-        #print(type(factspage[1]))
-        facts_table = factspage[1]
+def scrape_facts():
+    # Mars Facts with pandas scraping
+    # Set url
+    url = "https://space-facts.com/mars/"
+    factspage = pd.read_html(url)
+    #print(type(factspage[1]))
+    facts_table = factspage[1]
 
-        # Set index to Mars - Earth Comparison
-        facts_table.set_index("Mars - Earth Comparison", inplace=True)
-        facts_table.head()
-        facts_table.to_html("mars_earth_facts")
+    # Set index to Mars - Earth Comparison
+    facts_table.set_index("Mars - Earth Comparison", inplace=True)
+    facts_table.head()
+    facts_table.to_html("mars_earth_facts")
 
-        # Do I nedd to return anything here since I saved to html above
-        return
+    # Do I need to return anything here since I saved to html above
+    return
+
+
+def scrape_hemi_urls():
+    # Mars hemispheres images using splinter
+    # Set url to visit
+    hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+
+    #Browse to webpage
+    browser.visit(hemi_url)
+    # Use splinter to grab link
+    browser.click_link_by_partial_text('Cerberus')
+    #print(browser.find_by_text('Sample')['href'])
+    cerberus_link = browser.find_by_text('Sample')['href']
+
+    # #Browse to webpage
+    browser.visit(hemi_url)
+    # Use splinter to grab link
+    browser.click_link_by_partial_text('Schiaparelli')
+    #print(browser.find_by_text('Sample')['href'])
+    schiaparelli_link = browser.find_by_text('Sample')['href']
+
+    # #Browse to webpage
+    browser.visit(hemi_url)
+    # Use splinter to grab link
+    browser.click_link_by_partial_text('Syrtis')
+    #print(browser.find_by_text('Sample')['href'])
+    syrtis_link = browser.find_by_text('Sample')['href']
+
+    # #Browse to webpage
+    browser.visit(hemi_url)
+    # Use splinter to grab link
+    browser.click_link_by_partial_text('Valles')
+    #print(browser.find_by_text('Sample')['href'])
+    valles_link = browser.find_by_text('Sample')['href']
+
+    # Quit browser
+    browser.quit()
+
+    # print links
+    print(cerberus_link)
+    print(schiaparelli_link)
+    print(syrtis_link)
+    print(valles_link)
+
+    # Create dictionary of image values
+    # Create empty dict
+    hemisphere_image_urls = [
+        {"title": "Cerebus Hemisohere", "img_url": cerberus_link},
+        {"title": "Schiaparelli Hemisphere", "img_url": schiaparelli_link},
+        {"title": "Syrtis Major Hemisphere", "img_url": syrtis_link},
+        {"title": "Valles Marineris Hemisphere", "img_url": valles_link}    
+    ] 
+
+    return hemisphere_image_urls
