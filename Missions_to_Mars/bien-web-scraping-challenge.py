a85e431d1b1bef6baa4312452d0eba8de3a1a636
mcbien <mcbien1122@gmail.com>
2020-10-28 19:16:23 -0500

    All scraping is complete

--- Missions_to_Mars/bien-web-scraping-challenge.py
+++ Missions_to_Mars/bien-web-scraping-challenge.py
@@ -3,7 +3,7 @@
 
 # ### Michael Bien: web-scraping-challenge
 
-# In[115]:
+# In[33]:
 
 
 # import dependencies
@@ -12,7 +12,7 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 
 
-# In[116]:
+# In[34]:
 
 
 # Windows user
@@ -20,21 +20,21 @@ executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-# In[117]:
+# In[35]:
 
 
 # Retreive page with request
 #response = requests.get(url)
 
 
-# In[118]:
+# In[36]:
 
 
 # Create BeautifulSoup object
 # soup = BeautifulSoup(response.text, 'html.parser')
 
 
-# In[119]:
+# In[37]:
 
 
 # Headline and article
@@ -50,7 +50,8 @@ soup = BeautifulSoup(html, "html.parser")
 #Beautiful Soup
 # Print first list item
 first_headline = soup.find("li", class_="slide")
-news_title = first_headline.h3.text.strip()
+first_h = first_headline.find("div", class_="content_title")
+news_title = first_h.text.strip()
 
 article = soup.find("div", class_="article_teaser_body")
 news_p = article.text.strip()
@@ -59,14 +60,14 @@ news_p = article.text.strip()
 #browser.quit()
 
 
-# In[120]:
+# In[38]:
 
 
 print(news_title)
 print(news_p)
 
 
-# In[121]:
+# In[39]:
 
 
 # Define url to scrape
@@ -82,7 +83,7 @@ soup = BeautifulSoup(html, "html.parser")
 #print(soup)
 
 
-# In[122]:
+# In[40]:
 
 
 # Click Full Size
@@ -108,7 +109,7 @@ for box in a_fancybox:
 #browser.quit()
 
 
-# In[123]:
+# In[41]:
 
 
 #The image link is the last record returned
@@ -116,7 +117,7 @@ large_image_link = "http://www.jpl.nasa.gov" + image_link[-1]
 large_image_link
 
 
-# In[124]:
+# In[42]:
 
 
 # Mars Facts with pandas scraping
@@ -130,7 +131,7 @@ facts_table = factspage[1]
 #browser.quit()
 
 
-# In[125]:
+# In[43]:
 
 
 # Set index to Mars - Earth Comparison
@@ -138,52 +139,102 @@ facts_table.set_index("Mars - Earth Comparison", inplace=True)
 facts_table.head()
 
 
-# In[126]:
+# In[44]:
 
 
 # Convert dataframe to HTML
 facts_table.to_html("mars_earth_facts")
 
 
-# In[82]:
+# In[45]:
 
 
-# Mars hemispheres
+# ##ABANDONING THIS CELL AS IT IS NOT NECESSARY ##
+# # Mars hemispheres
+# # Set url to visit
+# hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+# browser.visit(hemi_url)
+
+# # Splinter
+# # Visit and parse web page
+# html = browser.html
+# soup = BeautifulSoup(html, "html.parser")
+    
+# #Beautiful Soup
+# # Print first list item
+# #first_headline = soup.find("li", class_="slide")
+
+# link_titles = soup.find_all("a", class_="itemLink product-item")
+# #len(link_titles)
+
+# # Create empty list to collect link titles
+
+
+# In[51]:
+
+
+# Mars hemispheres images using splinter
 # Set url to visit
 hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-
 #Browse to webpage
 browser.visit(hemi_url)
-
+# Use splinter to grab link
 browser.click_link_by_partial_text('Cerberus')
-print(browser.find_by_text('Sample')['href'])
+#print(browser.find_by_text('Sample')['href'])
+cerberus_link = browser.find_by_text('Sample')['href']
 
-html = browser.html
-soup = BeautifulSoup(html, "html.parser")
+# #Browse to webpage
+browser.visit(hemi_url)
+# Use splinter to grab link
+browser.click_link_by_partial_text('Schiaparelli')
+#print(browser.find_by_text('Sample')['href'])
+schiaparelli_link = browser.find_by_text('Sample')['href']
+
+# #Browse to webpage
+browser.visit(hemi_url)
+# Use splinter to grab link
+browser.click_link_by_partial_text('Syrtis')
+#print(browser.find_by_text('Sample')['href'])
+syrtis_link = browser.find_by_text('Sample')['href']
+
+# #Browse to webpage
+browser.visit(hemi_url)
+# Use splinter to grab link
+browser.click_link_by_partial_text('Valles')
+#print(browser.find_by_text('Sample')['href'])
+valles_link = browser.find_by_text('Sample')['href']
 
 # Quit browser
-# browser.quit()
+browser.quit()
 
-#print(soup)
-
-
-# In[76]:
-
-
-# Examine soup object
-a_tags = soup.find_all("a", href=True)
-#print([link for link in a_tags if "enhanced.tif" in link])
-print(a_tags)
+# print links
+print(cerberus_link)
+print(schiaparelli_link)
+print(syrtis_link)
+print(valles_link)
 
 
-# In[ ]:
+# In[53]:
 
 
-##  FINISH THIS PART
+# Create dictionary of image values
+# Create empty dict
+hemisphere_image_urls = [
+    {"title": "Cerebus Hemisohere", "img_url": cerberus_link},
+    {"title": "Schiaparelli Hemisphere", "img_url": schiaparelli_link},
+    {"title": "Syrtis Major Hemisphere", "img_url": syrtis_link},
+    {"title": "Valles Marineris Hemisphere", "img_url": valles_link}    
+]
 
 
-# In[104]:
+# In[54]:
+
+
+print(hemisphere_image_urls)
+
+
+# In[16]:
 
 
 #Import additional dependencies
@@ -191,7 +242,7 @@ import pymongo
 from pprint import pprint
 
 
-# In[105]:
+# In[17]:
 
 
 # Create connection to mongodb server
@@ -199,7 +250,7 @@ conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 
 
-# In[107]:
+# In[18]:
 
 
 # Declare the db in Mongo
@@ -208,7 +259,7 @@ db = client.MarsDB
 mars_collection = db.mars_material
 
 
-# In[ ]:
+# In[19]:
 
 
 mars_collection.insert_one(
